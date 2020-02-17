72708c2a2459e70edc09fb61eb7d598a9cd36e50
Alberto <torresalberto.r9@gmail.com>
2020-02-16 19:33:48 -0600

    Productora Creativa template
    html + schema + SEO optimized

---
+++ html/modal/contact.php
@@ -0,0 +1,51 @@
+<?php
+
+// Put contacting email here
+$php_main_email = "mailto:alberto.torres@1inkea.com;
+
+//Fetching Values from URL
+$php_name = $_POST['ajax_name'];
+$php_email = $_POST['ajax_email'];
+$php_message = $_POST['ajax_message'];
+
+
+
+//Sanitizing email
+$php_email = filter_var($php_email, FILTER_SANITIZE_EMAIL);
+
+
+//After sanitization Validation is performed
+if (filter_var($php_email, FILTER_VALIDATE_EMAIL)) {
+	
+	
+		$php_subject = "Mensaje recibido desde el sitio web";
+		
+		// To send HTML mail, the Content-type header must be set
+		$php_headers = 'MIME-Version: 1.0' . "\r\n";
+		$php_headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$php_headers .= 'From:' . $php_email. "\r\n"; // Sender's Email
+		$php_headers .= 'Cc:' . $php_email. "\r\n"; // Carbon copy to Sender
+		
+		$php_template = '<div style="padding:50px;">Hola ' . $php_name . ',<br/>'
+		. 'Gracias por contactarnos.<br/><br/>'
+		. '<strong style="color:#f00a77;">Nombre:</strong>  ' . $php_name . '<br/>'
+		. '<strong style="color:#f00a77;">Email:</strong>  ' . $php_email . '<br/>'
+		. '<strong style="color:#f00a77;">Mensaje:</strong>  ' . $php_message . '<br/><br/>'
+		. 'Este es un correo de confirmación de contacto.'
+		. '<br/>'
+		. 'Nos pondremos en contacto con usted tan pronto como sea posible .</div>';
+		$php_sendmessage = "<div style=\"background-color:#f5f5f5; color:#333;\">" . $php_template . "</div>";
+		
+		// message lines should not exceed 70 characters (PHP rule), so wrap it
+		$php_sendmessage = wordwrap($php_sendmessage, 70);
+		
+		// Send mail by PHP Mail Function
+		mail($php_main_email, $php_subject, $php_sendmessage, $php_headers);
+		echo "";
+	
+	
+} else {
+	echo "<span class='contact_error'>* Email Invalido *</span>";
+}
+
+?>
